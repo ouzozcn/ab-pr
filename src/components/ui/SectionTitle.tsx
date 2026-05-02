84780e60e99caa4e486b92c013c050ba9cb0b081
Oguzhan Ozcan <ouzmetu@gmail.com>
2026-05-02 19:37:02 +0300

    Update package dependencies to include framer-motion and motion, enhance SectionTitle component with new text animations and blur effects for improved visual presentation.

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -1,3 +1,6 @@
+import BlurText from "../BlurText";
+import TextType from "../TextType";
+
 interface SectionTitleProps {
 	title: string;
 	description?: string;
@@ -11,10 +14,21 @@ export default function SectionTitle({
 }: SectionTitleProps) {
 	return (
 		<div
-			className={`flex flex-col items-center md:items-start justify-center bg-(--brand-primary)  font-sans text-white text-lg md:text-2xl text-center md:text-start p-4 md:px-24 py-12 gap-4 md:gap-6 ${className}`}
+			className={`flex flex-col items-center md:items-start justify-center bg-(--brand-primary) lg:min-h-[208px] font-sans text-white text-lg md:text-2xl text-center md:text-start p-4 md:px-24 py-12 gap-4 md:gap-6 ${className}`}
 		>
-			<h2 className="text-lg md:text-6xl font-bold">{title}</h2>
-			<p className="text-md md:text-lg ">{description}</p>
+			<TextType
+				loop={false}
+				startOnVisible={true}
+				typingSpeed={100}
+				text={title || ""}
+				className="text-lg md:text-6xl font-bold"
+			/>
+			<BlurText
+				direction="bottom"
+				delay={100}
+				text={description || ""}
+				className="text-md md:text-lg"
+			/>
 		</div>
 	);
 }
